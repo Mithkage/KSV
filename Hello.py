6eabb53c5f2254b5f2f6534fe80c34db52a72813
Mithkage <58467243+Mithkage@users.noreply.github.com>
2022-09-27 17:47:44 +1000

    Adric added stuff

--- Hello.py
+++ Hello.py
@@ -4,12 +4,25 @@ import tkinter as tk
 from tkinter import filedialog
 import pandas as pd
 
-input("-- Press Enter to enable selection of Student Details --")
+# input("-- Press Enter to enable selection of Student Details --")
 file_path= filedialog.askopenfilename()
-f = open(file_path,"r")
-f = open(file_path,"r", encoding='ANSI', errors='ignore')
+# f = open(file_path,"r")
+# f = open(file_path,"r", encoding='ANSI', errors='ignore')
 
-csv_f= csv.reader(f)
+# csv_f= csv.reader(f)
+
+# with open(file_path, 'r', encoding='ANSI', errors='ignore') as infile:
+#     with open('my_file_utf8.csv', 'w') as outfile:
+#      outfile.write(infile.read())
+
+
+with open(file_path, 'r', encoding='ANSI', errors='ignore') as infile:
+    with open('my_file_utf8.csv', 'w', newline='') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+        for row in reader:
+            print(row)
+            writer.writerow(row)
 
 #Create Database
     # conn = sqlite3.connect(":memory:")
@@ -19,12 +32,13 @@ csv_f= csv.reader(f)
 
 
 
-firstRow = True
+# firstRow = True
 
-for row in csv_f:
-    if (firstRow):
-        firstRow = False
-    elif (not(row[7]=="" and row[8]=="")):
-        print(row)
+# for row in csv_f:
+#     if (firstRow):
+#         firstRow = False
+#     elif (not(row[7]=="" and row[8]=="")):
+#         print(row)
+
 
 
